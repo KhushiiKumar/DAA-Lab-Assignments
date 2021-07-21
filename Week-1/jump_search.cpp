914dbce418446a7c505ac4df895940d131b5c716
KhushiiKumar <68503114+KhushiiKumar@users.noreply.github.com>
2021-07-21 22:45:40 +0530

    Ques3 added

---
+++ Week-1/jump_search.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void jumpSearch(int arr[], int x, int n,int itr)
+{
+    int step=sqrt(n);
+    int prev=0;
+    while (arr[min(step, n)-1] < x)
+    {
+        prev = step;
+        step += sqrt(n);
+        itr++;
+        if (prev >= n)
+            return;
+    }
+    itr++;
+    while (arr[prev] < x)
+    {
+        prev++;
+        itr++;
+        if (prev == min(step, n))
+            return;
+    }
+    itr+=2;
+    if (arr[prev] == x)
+     cout<<"\nPresent "<<itr;
+    else
+     cout<<"\nNot Present "<<itr;
+     cout<<"\n";
+
+    return;
+}
+
+int main ()
+{
+    int t;
+    cout<<"Enter the number of test-cases:";
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cout<<"\nEnter the size of array: ";
+        cin>>n;
+
+        int a[n];
+
+        cout<<"Enter the elements:\n";
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+
+        int key;
+        cout<<"\nEnter element to be searched\n";
+        cin>>key;
+
+        int itr=0;
+        jumpSearch(a,key,n,itr);
+
+    }
+    return 0;
+}
+
+
+
+
+
